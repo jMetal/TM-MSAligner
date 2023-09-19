dcb82f0b4b32c4de3473117c520e85ecba9a2ab4
ajnebro <ajnebro@users.noreply.github.com>
2023-09-19 16:07:14 +0200

    Minor changes

--- src/main/java/org/uma/khaos/tm_msaligner/util/observer/TM_MSAFitnessWriteFileObserver.java
+++ src/main/java/org/uma/khaos/tm_msaligner/util/observer/TM_MSAFitnessWriteFileObserver.java
@@ -19,76 +19,79 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class TM_MSAFitnessWriteFileObserver<S extends TM_MSASolution> implements Observer<Map<String, Object>> {
+public class TM_MSAFitnessWriteFileObserver<S extends TM_MSASolution> implements
+    Observer<Map<String, Object>> {
 
-    private Integer evaluations ;
-    private final int plotUpdateFrequency ;
+  private Integer evaluations;
+  private final int plotUpdateFrequency;
 
-    private String fileNamePath;
+  private String fileNamePath;
 
-    /**
-     * Constructor
-     */
-    public TM_MSAFitnessWriteFileObserver(String fileNamePath, int plotUpdateFrequency) throws IOException {
+  /**
+   * Constructor
+   */
+  public TM_MSAFitnessWriteFileObserver(String fileNamePath, int plotUpdateFrequency)
+      throws IOException {
 
-        this.plotUpdateFrequency = plotUpdateFrequency ;
-        this.fileNamePath = fileNamePath;
+    this.plotUpdateFrequency = plotUpdateFrequency;
+    this.fileNamePath = fileNamePath;
+
+    try {
+      FileWriter fw = new FileWriter(fileNamePath);
+      BufferedWriter writer = new BufferedWriter(fw);
+      writer.write("Eval\tSOPwTP\tAlignedSegments\n");
+      writer.close();
+    } catch (IOException var7) {
+      throw new JMetalException("Error printing objectives to file: ", var7);
+    }
+  }
+
+  /**
+   * This method displays a front (population)
+   *
+   * @param data Map of pairs (key, value)
+   */
+  @Override
+  public void update(Observable<Map<String, Object>> observable, Map<String, Object> data) {
+    evaluations = (Integer) data.get("EVALUATIONS");
+    List<S> population = (List) data.get("POPULATION");
+
+    if (evaluations != null && population != null) {
+      if (evaluations % plotUpdateFrequency == 0) {
+
+        List<Double> scores1 = (List) population.stream().map((s) -> {
+          return s.objectives()[0] * -1.0;
+        }).collect(Collectors.toList());
+        List<Double> scores2 = (List) population.stream().map((s) -> {
+          return s.objectives()[1] * -1.0;
+        }).collect(Collectors.toList());
 
         try {
-                FileWriter fw = new FileWriter(fileNamePath);
-                BufferedWriter writer = new BufferedWriter(fw);
-                writer.write("Eval\tSOPwTP\tAlignedSegments\n");
-                writer.close();
+          String textToAppend = evaluations + "\t" +
+              String.format("%.2f", Collections.max(scores1)) + "\t" +
+              String.format("%.2f", Collections.max(scores2));
+
+          try (FileWriter fw = new FileWriter(fileNamePath, true);
+              BufferedWriter writer = new BufferedWriter(fw);) {
+            writer.write(textToAppend);
+            writer.write(System.lineSeparator());
+          }
+
         } catch (IOException var7) {
-            throw new JMetalException("Error printing objectives to file: ", var7);
+          throw new JMetalException("Error printing objectives to file: ", var7);
         }
+
+      }
+    } else {
+      JMetalLogger.logger.warning(getClass().getName() +
+          " : insufficient for generating real time information." +
+          " Either EVALUATIONS or BEST_SOLUTION keys have not been registered yet by the algorithm");
     }
+  }
 
-    /**
-     * This method displays a front (population)
-     * @param data Map of pairs (key, value)
-     */
-    @Override
-    public void update(Observable<Map<String, Object>> observable, Map<String, Object> data) {
-        evaluations = (Integer)data.get("EVALUATIONS") ;
-        List<S> population = (List)data.get("POPULATION");
-
-        if (evaluations!=null && population!=null) {
-            if (evaluations%plotUpdateFrequency == 0){
-
-                List<Double> scores1 = (List)population.stream().map((s) -> {
-                    return s.objectives()[0]*-1.0;
-                }).collect(Collectors.toList());
-                List<Double> scores2 = (List)population.stream().map((s) -> {
-                    return s.objectives()[1]*-1.0;
-                }).collect(Collectors.toList());
-
-                try {
-                    String textToAppend = evaluations + "\t"+
-                            String.format("%.2f", Collections.max(scores1))  + "\t" +
-                            String.format("%.2f", Collections.max(scores2));
-
-                    try(FileWriter fw = new FileWriter(fileNamePath, true);
-                        BufferedWriter writer = new BufferedWriter(fw);) {
-                            writer.write(textToAppend);
-                            writer.write( System.lineSeparator());
-                        }
-
-                } catch (IOException var7) {
-                    throw new JMetalException("Error printing objectives to file: ", var7);
-                }
-
-            }
-        } else {
-            JMetalLogger.logger.warning(getClass().getName()+
-                    " : insufficient for generating real time information." +
-                    " Either EVALUATIONS or BEST_SOLUTION keys have not been registered yet by the algorithm");
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "FicheroValoresMaximos" ;
-    }
+  @Override
+  public String toString() {
+    return "FicheroValoresMaximos";
+  }
 }
 
